773e1a2b76ea95e468f5cb83f89de7374a413e34
Onrawee Kaekratoke <onrawee.k@github.com>
2019-03-04 02:51:03 +0700

    Update Profile.php

--- Profile.php
+++ Profile.php
@@ -1,5 +1,20 @@
 <!DOCTYPE html>
-
+<?php
+   include("ConnectDB.php");
+   if(session_start()){
+   if($_SESSION['login_status'] == 1) {
+      // username and password sent from form 
+      $user = $_SESSION['login_user'];
+      $sql = "SELECT fname, lname, email FROM user_master WHERE user = '$user'";	  
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+	  
+   }else{
+	   $_SESSION['login_status'] = 0;
+	   header("location: Homepage.php");
+   }
+   }
+?>
 <html>
 	<head>
 		<title>Lost & Found</title>
@@ -22,6 +37,9 @@
 							<a href="ManagePost.php"  class="menustyle">Manage Post</a>
 						</li>
 						<li>
+							<a href="UpdateNotice.php"  class="menustyle">Create new post</a>
+						</li>
+						<li>
 							<a href="Profile.php"  class="menustyle">Profile</a>
 						</li>
 						<li>
@@ -34,7 +52,37 @@
 			<!--Form Area-->
 			<div class="FormArea">
 				<h1>Profile</h1></br>
-			
+				<table class="inputform">
+							<tr>
+								<td class="topic"></td>
+								<td class="input"></td>
+							</tr>
+							<tr>
+								<td class="topic">First name</td>
+								<td class="input">
+									<? echo $row['fname'];?>
+								</td>
+							</tr>
+							<tr>
+								<td class="topic">Last name</td>
+								<td class="input">
+									<? echo $row['lname'];?>
+								</td>
+							</tr>
+							<tr>
+								<td class="topic">Email</td>
+								<td class="input">
+									<? echo $row['email'];?>
+								</td>
+							</tr>
+							<tr>
+								<td class="topic">Username</td>
+								<td class="input">
+									<? echo $user;?>
+								</td>
+							</tr>
+							
+						</table>
 			</div>
 		</div>		
 	</body>
