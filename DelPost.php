22e43d5b1b90361e301629de4a803ac3da6332c7
Onrawee Kaekratoke <onrawee.k@github.com>
2019-03-23 01:49:47 +0700

    Delete DelPost.php

--- DelPost.php
+++
@@ -1,9 +0,0 @@
-<?php
-   include("ConnectDB.php");
-   session_start();
-   $posid = $_REQUEST["id"];
-   $sqldel = "DELETE FROM post_master where postid = '$posid'"; 
-   $delresult = mysqli_query($db,$sqldel);
-   $posid = 0;
-   header("location: ManagePost.php");
-?>
